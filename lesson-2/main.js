5c6faafd6cc5c3d69dd6ef94448e32ea8908b265
Ayatullo <aatullorahmatullaev@gmail.com>
2023-12-01 10:53:04 +0000

    add js-2

---
+++ lesson-2/main.js
@@ -0,0 +1,40 @@
+// Типы Данных 8
+// 1 strig - строка
+// ключевые слова var let const 
+// переменные - озгормо
+// операторы (=,==,===,+,-,/.*,>< >= <=, )
+var carname = "Fit"
+var laptopname ='Nurshoola'
+// 2 number
+var applePrice2 = 100
+var batteryMass = 70
+// 3 boolean
+var isSnow = false
+var isSnow2 = true
+// 4 null
+var baha = null
+baha = 'baha'
+// 5 undefined
+var jok;
+// 6 ---
+var id = Symbol("userid")
+// 7 -- bigint
+var maxKm =100n
+// 8 object object{}, Array[]
+var headerMenu = ['home', 'about', 'Contacts']
+var table={
+    // key:value
+    color:'red',
+    height:2,
+    width:3,
+    matterial:'metal'
+}
+var user={
+    name:'kolbai',
+    ismarried:false,
+    salary:500000,
+    age:20,
+    car:null,
+}
+
+
