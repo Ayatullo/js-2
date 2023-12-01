85e5915e2fee0e98961da570094504465c17501e
Ayatullo <aatullorahmatullaev@gmail.com>
2023-12-01 11:53:20 +0000

    add movie object

--- lesson-2/main.js
+++ lesson-2/main.js
@@ -36,5 +36,24 @@ var user={
     age:20,
     car:null,
 }
+var baby = {
+    image: 'http://www.nurcinema.kg/upload/movies/movie_1203/thumb.jpg',
+    movieName: "Малыш",
+    year: 2023,
+    author: "Учкун Жакыпбеков",
+    description: `
+    Талант отчаянный хулиган из провинциального городка Балыкчы.  Однаждны в очередной драке он пулучает травму головы и умственно становится настоящим младенцем. Маме Таланта Толгонай приходится воспитывать своего большого сына с самого начало и сделать его снова взрослым человеком, но к ней на помощ приходит Бакыт, который давно женился бы на Толгонай, если бы не Талант.`,
+    country: "Кыргызстан",
+    genre: 'Комедия, драма',
+    roles: ['Аскат Сулайманов',' Шумкарбек Бейшеналиев', 'Элмира Тагаева', 'Адеми Бейшенбекова']
+  }
 
+  document.write(`<img src='${baby.image}'alt='image' />`)
+  document.write(`<h3>${baby.movieName}</h3>`)
+  document.write(`<h3>${baby.year} - жыл </h3>`)
+  document.write(`<h3>${baby.country}</h3>`)
+  document.write(`<h3>${baby.author}</h3>`)
+  document.write(`<h3>${baby.roles[0]}</h3>`)
+  document.write(`<h3>${baby.description}</h3>`)
+  
 
